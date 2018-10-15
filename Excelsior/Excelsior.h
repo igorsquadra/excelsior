3ec34939959ed84bccb82efbffdbf9faeb8a8d41
Igor Squadra <igor.squadra@21ilab.com>
2018-10-15 15:21:53 +0200

    Initial Commit

---
+++ Excelsior/Excelsior.h
@@ -0,0 +1,19 @@
+//
+//  Excelsior.h
+//  Excelsior
+//
+//  Created by Igor Squadra on 15/10/2018.
+//  Copyright © 2018 Igor Squadra. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Excelsior.
+FOUNDATION_EXPORT double ExcelsiorVersionNumber;
+
+//! Project version string for Excelsior.
+FOUNDATION_EXPORT const unsigned char ExcelsiorVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Excelsior/PublicHeader.h>
+
+
